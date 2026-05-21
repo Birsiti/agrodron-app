b07a58bae7da8ce280c70ae14d3e7daa50f2abe1
Birsiti <36166538+Birsiti@users.noreply.github.com>
2026-05-21 08:46:43 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,7 @@
 // ══════════════════════════════════════════════════════════
 
 // ── Apps Script ────────────────────────────────────────────
-const SCRIPT_URL     = 'https://script.google.com/macros/s/AKfycbxWZ0Z7-ZzIS22kxR3PkblCAcB7hNEQkQShcOuBYUNlxpHT7VFQD8WLTArs5QAXDg0Q/exec';
-
+const SCRIPT_URL     = 'https://script.google.com/macros/s/AKfycbxWZ0Z7-ZzlS22kxR3PkblCAcB7hNEQkQShcOuBYUNlxpHT7VFQD8WLTArs5QAXDg0Q/exec';
 // ── Google Sheets ──────────────────────────────────────────
 const SHEETS_KEY     = 'AIzaSyAxiaO9KgDN277ibcLCfXHAc2VpSSeQMDg';
 const SHEET_ID       = '1NzeC5jdm5osCHFQVSHKpUGMkWT2xI6xTjwl7LMRLHWU';
